a76f47777cca025a51d8d3f8d5d247f8863bd259
oscar rene peñaloza ardila <oscar.penaloza@est.iudigital.edu.co>
2026-03-28 13:51:11 -0500

    cambio for

--- src/modelo/Pedido.java
+++ src/modelo/Pedido.java
@@ -5,14 +5,18 @@ import java.util.List;
 
 public class Pedido {
     private Cliente cliente;
-    private List<Producto> productos;
+    private List<Producto> productos;// relacion de asociacion de un pedido con varios productos
     public Pedido(Cliente cliente, List<Producto> productos) {
         this.cliente = cliente;
         this.productos = productos;
     }
 
     public Double calcularTotal() {
-       return productos.stream().mapToDouble(Producto::getPrecio).sum();
+        Double total = 0.0;
+        for (Producto producto : productos) {
+            total += producto.getPrecio();
+        }
+        return total;
     }
     
     public Cliente getCliente() {
